74e28bf4fb895fe3c92b8bd102c705bef3c584b8
Anton-Zarubin <a-zarubin@mail.ru>
2025-05-03 12:23:37 +0300

    add gateway

---
+++ gateway/src/main/java/org/example/gateway/config/GatewayConfig.java
@@ -0,0 +1,26 @@
+package org.example.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gateway.security.AuthenticationFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class GatewayConfig {
+
+    private final AuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        "auth_route", r -> r.path("/auth-service/**")
+                                .filters(f -> f.filter(filter))
+                                .uri("lb://AUTH-SERVICE")
+                )
+                .build();
+    }
+}
